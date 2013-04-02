504121f9c460068076811879bf1e5b45c5627a0f
Ruslan Osmanov <rrosmanov@gmail.com>
2013-04-02 12:04:58 +0500

    Fix: forgotten arg of EventBuffer::write

--- classes/buffer.c
+++ classes/buffer.c
@@ -613,7 +613,7 @@ PHP_METHOD(EventBuffer, write)
 	long                 howmuch = -1;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "Z|l",
-				&ppzfd) == FAILURE) {
+				&ppzfd, &howmuch) == FAILURE) {
 		return;
 	}
 
