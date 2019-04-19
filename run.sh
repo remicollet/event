6fda57cbc0418dae238c4709731c1756ba2e5b1f
Ruslan Osmanov <rrosmanov@gmail.com>
2019-04-19 11:53:46 +0300

    Dev: run.sh now checks for sockets.so

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -
+
+sockets_so_path="$(php-config --extension-dir)/sockets.so"
+if [ -e "$sockets_so_path" ]; then
+  cp "$sockets_so_path" ./.libs/
+fi
+
 MALLOC_PERTURB_=$(($RANDOM % 255 + 1)) \
 	MALLOC_CHECK_=3 \
 	USE_ZEND_ALLOC=0 \
 	ZEND_DONT_UNLOAD_MODULES=1 \
-	php -n -d extension=event.so  -dextension_dir=./.libs "$@"
+	php -n -dextension=sockets.so -dextension=event.so  -dextension_dir=./.libs "$@"
