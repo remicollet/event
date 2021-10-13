7ea01b34a7d2e650da8b8099474343c77a7a5ac7
Ruslan Osmanov <rrosmanov@gmail.com>
2021-10-14 02:07:09 +0300

    Added posix ext to run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,15 @@ if [ -e "$sockets_so_path" ]; then
   sockets_option='-dextension=sockets.so'
 fi
 
+posix_so_path="$(php-config --extension-dir)/posix.so"
+posix_option=
+if [ -e "$posix_so_path" ]; then
+  cp "$posix_so_path" ./.libs/
+  posix_option='-dextension=posix.so'
+fi
+
 MALLOC_PERTURB_=$(($RANDOM % 255 + 1)) \
 	MALLOC_CHECK_=3 \
 	USE_ZEND_ALLOC=0 \
 	ZEND_DONT_UNLOAD_MODULES=1 \
-	php -n $sockets_option -dextension=event.so  -dextension_dir=./.libs "$@"
+	php -n $posix_option $sockets_option -dextension=event.so  -dextension_dir=./.libs "$@"
