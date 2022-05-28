53517f8cd825ed3fd786e24c04cc5cf8b2c364df
Ruslan Osmanov <608192+rosmanov@users.noreply.github.com>
2022-05-28 10:37:09 +0400

    Internal: replaced sed with perl in setver.sh
    sed did not work well on macos

--- setver.sh
+++ setver.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -
 # Set PECL package version
 
+set -e
+
 if [[ $# < 1 ]]; then
 	echo >&2 'Version string expected'
 	exit 1
@@ -8,7 +10,7 @@ fi
 
 php_event_version="$1"
 
-for subdir in php5 php7 php8
-do
-	sed -i -r 's/(# *define *PHP_EVENT_VERSION\ +").*"/\1'$php_event_version'"/g' $subdir/php_event.h
-done
+perl -pi -e 's/(# *define *PHP_EVENT_VERSION\ +).*/$1"'$php_event_version'"/g' php{5,7,8}/php_event.h
+
+echo "Done"
+echo "(!) Don't forget to update package.xml"
